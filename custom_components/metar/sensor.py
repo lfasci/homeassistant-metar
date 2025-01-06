396e1bbc96a34a85eb17f1866cb6ecff45c2ca4a
Mrkoff <79903266+Mrkoff@users.noreply.github.com>
2025-01-06 12:27:11 +0100

    Update sensor.py
    Updated sensor.py to work in 2025.1.0 Core update.

--- custom_components/metar/sensor.py
+++ custom_components/metar/sensor.py
@@ -1,15 +1,14 @@
-import logging, time
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+import logging
 from datetime import timedelta
-from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
+import voluptuous as vol
+from aiohttp import ClientSession
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.typing import HomeAssistantType, ConfigType
+from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
 from homeassistant.util import Throttle
-from homeassistant.const import ATTR_ATTRIBUTION, ATTR_TIME, CONF_MONITORED_CONDITIONS, TEMP_CELSIUS
-try:
-    from urllib2 import urlopen
-except:
-    from urllib.request import urlopen
+from homeassistant.const import CONF_MONITORED_CONDITIONS
+import homeassistant.helpers.config_validation as cv
 from metar import Metar
 
 DOMAIN = 'metar'
@@ -21,47 +20,51 @@ BASE_URL = "https://tgftp.nws.noaa.gov/data/observations/metar/stations/"
 _LOGGER = logging.getLogger(__name__)
 
 SENSOR_TYPES = {
-    'time': ['Updated ', None],
+    'time': ['Updated', None],
     'weather': ['Condition', None],
     'temperature': ['Temperature', 'C'],
     'wind': ['Wind speed', None],
-    'pressure': ['Pressure', None],
-    'visibility': ['Visibility', None],
-    'precipitation': ['Precipitation', None],
+    'pressure': ['Pressure', 'hPa'],
+    'visibility': ['Visibility', 'm'],
     'sky': ['Sky', None],
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_AIRPORT_NAME): cv.string,
     vol.Required(CONF_AIRPORT_CODE): cv.string,
-    vol.Optional(CONF_MONITORED_CONDITIONS, default=[]):
-        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),	
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=[]): vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
 })
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-   airport = {'location': str(config.get(CONF_AIRPORT_NAME)), 'code': str(config.get(CONF_AIRPORT_CODE))}
+async def async_setup_platform(hass: HomeAssistantType, config: ConfigType, async_add_entities, discovery_info=None):
+    """Set up the METAR sensor platform."""
+    airport = {
+        'location': str(config[CONF_AIRPORT_NAME]),
+        'code': str(config[CONF_AIRPORT_CODE]),
+    }
 
-   data = MetarData(airport)
-   dev = []
-   for variable in config[CONF_MONITORED_CONDITIONS]:
-       dev.append(MetarSensor(airport, data, variable, SENSOR_TYPES[variable][1]))
-   add_entities(dev, True)
+    session = async_get_clientsession(hass)
+    data = MetarData(airport, session)
+    sensors = [
+        MetarSensor(airport, data, sensor_type, SENSOR_TYPES[sensor_type][1])
+        for sensor_type in config[CONF_MONITORED_CONDITIONS]
+    ]
 
+    async_add_entities(sensors, True)
 
 class MetarSensor(Entity):
-
-    def __init__(self, airport, weather_data, sensor_type, temp_unit):
-       self._state = None
-       self._name = SENSOR_TYPES[sensor_type][0]
-       self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
-       self._airport_name = airport["location"]
-       self.type = sensor_type
-       self.weather_data = weather_data
+    def __init__(self, airport, weather_data, sensor_type, unit_of_measurement):
+        """Initialize the sensor."""
+        self._state = None
+        self._name = SENSOR_TYPES[sensor_type][0]
+        self._unit_of_measurement = unit_of_measurement
+        self._airport_name = airport["location"]
+        self.type = sensor_type
+        self.weather_data = weather_data
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name + " " + self._airport_name;
+        return f"{self._name} {self._airport_name}"
 
     @property
     def state(self):
@@ -73,24 +76,20 @@ class MetarSensor(Entity):
         """Return the unit of measurement."""
         return self._unit_of_measurement
 
-    def update(self):
-        """Get the latest data from Metar and updates the states."""
+    async def async_update(self):
+        """Fetch the latest data from METAR."""
+        await self.weather_data.async_update()
 
-        try:
-          self.weather_data.update()
-        except URLCallError:
-          _LOGGER.error("Error when retrieving update data")
-          return
-
-        if self.weather_data is None:
+        if not self.weather_data.sensor_data:
+            self._state = None
             return
 
         try:
             if self.type == 'time':
-                 self._state = self.weather_data.sensor_data.time.ctime()
-            if self.type == 'temperature':
-                 degree = self.weather_data.sensor_data.temp.string().split(" ")
-                 self._state = degree[0]
+                self._state = self.weather_data.sensor_data.time.ctime()
+            elif self.type == 'temperature':
+                degree = self.weather_data.sensor_data.temp.string().split(" ")
+                self._state = degree[0]
             elif self.type == 'weather':
                 self._state = self.weather_data.sensor_data.present_weather()
             elif self.type == 'wind':
@@ -99,44 +98,34 @@ class MetarSensor(Entity):
                 self._state = self.weather_data.sensor_data.press.string("mb")
             elif self.type == 'visibility':
                 self._state = self.weather_data.sensor_data.visibility()
-                self._unit_of_measurement = 'm'
-            # elif self.type == 'precipitation':
-                # self._state = self.weather_data.sensor_data.precip_1hr.string("in")
-                # self._unit_of_measurement = 'mm'
             elif self.type == 'sky':
-               self._state = self.weather_data.sensor_data.sky_conditions("\n     ")
+                self._state = self.weather_data.sensor_data.sky_conditions("\n     ")
         except KeyError:
             self._state = None
-            _LOGGER.warning(
-                "Condition is currently not available: %s", self.type)
+            _LOGGER.warning("Condition is currently not available: %s", self.type)
 
 class MetarData:
-    def __init__(self, airport):
-       """Initialize the data object."""
-       self._airport_code = airport["code"]
-       self.sensor_data = None
-       self.update()
+    def __init__(self, airport, session: ClientSession):
+        """Initialize the METAR data object."""
+        self._airport_code = airport["code"]
+        self._session = session
+        self.sensor_data = None
 
     @Throttle(SCAN_INTERVAL)
-    def update(self):
-        url = BASE_URL + self._airport_code + ".TXT"
+    async def async_update(self):
+        """Fetch data from the METAR API."""
+        url = f"{BASE_URL}{self._airport_code}.TXT"
         try:
-            urlh = urlopen(url)
-            report = ''
-            for line in urlh:
-                if not isinstance(line, str):
-                    line = line.decode() 
-                if line.startswith(self._airport_code):
-                    report = line.strip()
-                    self.sensor_data = Metar.Metar(line)
-                    _LOGGER.info("METAR ",self.sensor_data.string())
-                    break
-            if not report:
-                _LOGGER.error("No data for ",self._airport_code,"\n\n")
-        except Metar.ParserError as exc:
-            _LOGGER.error("METAR code: ",line)
-            _LOGGER.error(string.join(exc.args,", "),"\n")
-        except:
-            import traceback
-            _LOGGER.error(traceback.format_exc())
-            _LOGGER.error("Error retrieving",self._airport_code,"data","\n")
+            async with self._session.get(url) as response:
+                response.raise_for_status()
+                text = await response.text()
+
+                for line in text.splitlines():
+                    if line.startswith(self._airport_code):
+                        self.sensor_data = Metar.Metar(line)
+                        _LOGGER.info("METAR Data: %s", self.sensor_data.string())
+                        return
+
+                _LOGGER.error("No METAR data found for %s", self._airport_code)
+        except Exception as exc:
+            _LOGGER.error("Error retrieving METAR data for %s: %s", self._airport_code, exc)
